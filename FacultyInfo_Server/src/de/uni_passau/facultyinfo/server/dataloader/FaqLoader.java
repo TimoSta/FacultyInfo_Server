834f3dbacd44a9e4d45045ff1905e704704dac0a
Timo Staudinger <staudinger.timo@gmail.com>
2014-01-18 00:00:04 +0100

    Improved Formatting of News & FAQ. Links are now included.

--- FacultyInfo_Server/src/de/uni_passau/facultyinfo/server/dataloader/FaqLoader.java
+++ FacultyInfo_Server/src/de/uni_passau/facultyinfo/server/dataloader/FaqLoader.java
@@ -1,7 +1,10 @@
 package de.uni_passau.facultyinfo.server.dataloader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -53,8 +56,41 @@ public class FaqLoader {
 					Connection subConnection = Jsoup.connect(url);
 					subConnection.ignoreContentType(true);
 					Document subDoc = subConnection.get();
-					String content = subDoc.select("table.contentpaneopen")
-							.get(1).text();
+					subDoc.outputSettings(new Document.OutputSettings()
+							.prettyPrint(false));
+					subDoc.select("br").append("\\n");
+					subDoc.select("p").prepend("\\n");
+					subDoc.select("li").prepend("\\n");
+					Element contentElement = subDoc
+							.select("table.contentpaneopen").get(1)
+							.select("tr").get(0);
+					contentElement.select("table").html("");
+					String content = contentElement.html();
+					ArrayList<String> urls = new ArrayList<String>();
+					for (Element element : contentElement.select("a")) {
+						urls.add(element.absUrl("href"));
+					}
+					Pattern p = Pattern
+							.compile("<a .*?href=\".*?\" .*?>(.*?)</a>");
+					Matcher m = p.matcher(content);
+					StringBuffer s = new StringBuffer();
+					int i = 0;
+					while (m.find()) {
+						m.appendReplacement(s,
+								urls.size() <= i
+										|| Jsoup.parse(m.group(1)).text()
+												.trim().isEmpty() ? m.group(1)
+										: m.group(1) + " (" + urls.get(i)
+												+ ") ");
+						i++;
+					}
+					m.appendTail(s);
+
+					content = Jsoup
+							.parse(s.toString()
+									.replaceAll("\\\\n", "[newline]")).text()
+							.replace("[newline]", "\n").trim();
+
 					Faq faq = new Faq(faqId, faqCategory, faqTitle, content);
 					result += " -- " + faq.getTitle();
 					if (faqDAO.createFaq(faq)) {
--- FacultyInfo_Server/src/de/uni_passau/facultyinfo/server/dataloader/NewsLoader.java
+++ FacultyInfo_Server/src/de/uni_passau/facultyinfo/server/dataloader/NewsLoader.java
@@ -3,9 +3,12 @@ package de.uni_passau.facultyinfo.server.dataloader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -36,18 +39,42 @@ public class NewsLoader {
 			for (Element element : newsElements) {
 				String id = UUID.randomUUID().toString();
 				String title = element.select("title").get(0).text();
-				// String description = element.select("description").get(0)
-				// .text();
 				String url = element.select("link").get(0).text();
 				SimpleDateFormat sdf = new SimpleDateFormat(
 						"E, d MMM yyyy H:m:s Z", Locale.ENGLISH);
-				Element textElement = Jsoup.connect(url).get()
-						.select("table.contentpaneopen").get(1).select("td")
-						.get(0);
+				Document subDoc = Jsoup.connect(url).get();
+				subDoc.outputSettings(new Document.OutputSettings()
+						.prettyPrint(false));
+				subDoc.select("br").append("\\n");
+				subDoc.select("p").prepend("\\n");
+				subDoc.select("li").prepend("\\n");
+				Element textElement = subDoc.select("table.contentpaneopen")
+						.get(1).select("td").get(0);
 				textElement.select("table").get(0).html("");
-				String text = Jsoup.parse(
-						textElement.html().replace("<br>", "\n")
-								.replace("<br />", "\n")).text();
+				String text = textElement.html();
+				ArrayList<String> urls = new ArrayList<String>();
+				for (Element linkElement : textElement.select("a")) {
+					urls.add(linkElement.absUrl("href"));
+				}
+				Pattern p = Pattern.compile("<a .*?href=\".*?\" .*?>(.*?)</a>");
+				Matcher m = p.matcher(text);
+				StringBuffer s = new StringBuffer();
+				int i = 0;
+				while (m.find()) {
+					m.appendReplacement(
+							s,
+							urls.size() <= i
+									|| Jsoup.parse(m.group(1)).text().trim()
+											.isEmpty() ? m.group(1) : m
+									.group(1) + " (" + urls.get(i) + ") ");
+					i++;
+				}
+				m.appendTail(s);
+
+				text = Jsoup
+						.parse(s.toString().replaceAll("\\\\n", "[newline]"))
+						.text().replace("[newline]", "\n").trim();
+
 				String description = text.length() >= 200 ? text.substring(0,
 						199) + "..." : text;
 
